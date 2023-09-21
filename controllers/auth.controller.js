dc29c4e2da76154349554ff212476b39db12e66f
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-09-21 11:59:25 +0530

    server established

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,11 +2,16 @@ function getSignup(req,res){
     res.render('../views/customer/auth/signup');
 }
 
+function signup(req,res){
+    
+}
+
 function getLogin(req,res){
     res.render('../views/customer/auth/login');
 }
 
 module.exports={
     getSignup:getSignup,
+    signup:signup,
     getLogin:getLogin
 }
