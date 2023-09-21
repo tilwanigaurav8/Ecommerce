d48ef6dfb7137a00bb5d169ae3f3fea1273741b0
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-09-21 17:22:05 +0530

    sessions introduced

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const path=require('path');
 const express=require('express');
 
+const csrf=require('csurf');
+const expressSession=require('express-session');
+
+const createSessionConfig=require('./config/session');
 const db=require('./data/database')
+const addCsrfTokenMiddleware=require('./middlewares/csrf-tokens');
+const errorHandlerMiddleware=require('./middlewares/error-handler')
 const authRoutes=require('./routes/auth.routes');
 
 const app=express();
@@ -11,8 +17,14 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 
-app.use('/',authRoutes);
+const sessionConfig=createSessionConfig();
+app.use(expressSession(sessionConfig));
+app.use(csrf());
 
+app.use(addCsrfTokenMiddleware)
+
+app.use('/',authRoutes);
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase().then(function(){ 
     app.listen(3000);
