350ab086bd926eae3fa7b6b0c1f1ae9cc6d938f4
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-10-03 03:56:24 +0530

    image now being displlayed from admin end

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
-const adminRoutes=require('./routes/admin.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 const app = express();
 
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
 
 const sessionConfig = createSessionConfig();
@@ -33,7 +34,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
