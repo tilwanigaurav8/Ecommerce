87b1c55f774ec6e656be31fcc10e32b7cdcfca5d
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-10-03 02:26:47 +0530

    image upload functionality added

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -6,7 +6,11 @@ function getProducts(req, res) {
     res.render('admin/products/new-product');
   }
   
-  function createNewProduct() {}
+  function createNewProduct(req,res) {
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect('/admin/products');
+  }
   
   module.exports = {
     getProducts: getProducts,
